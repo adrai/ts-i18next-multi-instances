f1ff0cb924158cda03d2d7d79f4de3bd63b57c0d
Adriano Raiano <adriano@raiano.ch>
2023-06-20 13:47:11 +0200

    comment

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import i18next from './i18n';
 import otheri18n from './otheri18n';
 
 console.log(i18next.t('title'));
+
+// this works in js, but not in ts
 console.log(otheri18n.t('hi'));
+
+// and this works in ts, but should not
+console.log(otheri18n.t('title'));
